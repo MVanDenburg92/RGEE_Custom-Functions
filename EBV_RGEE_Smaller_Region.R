5241c505a1b176640a4e9a2d3c0d701cab9a9649
Miles Van Denburg <54696014+MVanDenburg92@users.noreply.github.com>
2020-11-11 03:46:30 -0500

    Created Function which ccreates the dataframe for the EVI values.

---
+++ EBV_RGEE_Smaller_Region.R
@@ -0,0 +1,482 @@
+
+library(greenbrown)
+library(sf)
+library(sp)
+library(tidyverse)
+
+library(raster)
+library(qdapRegex)
+library(lubridate)
+
+library(doParallel)  #Foreach Parallel Adaptor 
+library(foreach)
+library(parallel)
+
+# devtools::install_github("rstudio/reticulate")
+
+#Install if you don't have already
+# remotes::install_github("r-spatial/rgee")
+
+
+library(rgee)
+
+
+# ee_install()
+ee_Initialize(email = 'miles.vandenburg@gmail.com')
+
+library(reticulate)
+
+# ee_check()
+
+# install.packages(c('processx','ps'))
+# packageVersion('rgee')
+# rgee::ee_check()
+# reticulate::py_config()
+
+# ee_clean_pyenv()
+# ee_clean_credentials()
+# ee_Initialize()
+
+
+# ee = import("ee")          # Import the Earth Engine library
+# ee$Initialize() 
+
+
+
+#Bring on the RGEE Objects and Vectors
+
+#Smaller ecoregion
+
+
+x = 'Ecoregion_Small.shp'
+band_name = 'EVI'
+img_collection = "LANDSAT/LT05/C01/T1_32DAY_EVI"
+start = "2000-01-01"
+end = "2008-01-01"
+  
+gee_extract <- function(x = 'Ecoregion_Small.shp', band_name = 'EVI', img_collection = "LANDSAT/LT05/C01/T1_32DAY_EVI", start = "2000-01-01", end = "2008-01-01") {
+  
+  eco_mask2 <- st_read(x,quiet = TRUE) %>% st_transform(4326) 
+  
+  # plot(eco_mask2)
+  # 
+  eco_mask_ee2 <- sf_as_ee(eco_mask2)
+
+  
+  ##Bring in the imagery from RGEE
+  # 
+  s4 <- ee$ImageCollection(img_collection)
+  # start <- '2000-01-01'
+  # end <- '2008-01-01'
+  # 
+  s4 <-  s4$filterDate(ee$Date(start),ee$Date(end))$filterBounds(eco_mask_ee2)
+  
+  # s4info1 <- s4$   getInfo()
+  
+  # band_name <- s4info1[["bands"]][[1]][["id"]]
+  
+  
+  #Show the amount of images in the collection
+  nbrImages_s4 = s4$size() %>% ee$Number()
+  
+  
+  #Mapping the clip function over the collection
+  s4 = s4$map(function(image){image$clip(eco_mask_ee2)})
+  
+  # s4info2 <- s4$getInfo()
+  
+  #Selecting the EVI band which holds the information
+  s4 = s4$select(band_name)
+  
+  #Get information about the collection 
+  # s4info3 <- s4$getInfo()
+  
+  
+  
+  
+  ## SMALLER REGIONS
+  
+  #Get setup for the loop.  In case the loop crashes or doesn't work, you must rerun the above chunk first before running this chunk again. 
+  
+  np = import("numpy")      # Import Numpy 
+  
+  
+  # nimages2 <- s4$size()$getInfo()  
+  
+  nimages2 <- s4 %>% ee$FeatureCollection$size() %>% ee$Number()
+  ic_date2 <- ee_get_date_ic(s4)
+  
+  
+  s4_img_list <- list()
+  latlng2 <- list()
+  lats2 <- list()
+  lngs2 <- list()
+  evi_values4 <- list()
+  s4_names <- list()
+  
+  evi_values5 <- list()
+  evi_values6 <- list()
+  
+  # nimages2$getInfo()
+
+
+  for(i in seq(nimages2$getInfo())){
+    # as.numeric(i)
+    py_index <- i - 1
+    s4_img <- ee$Image(s4 %>% ee$FeatureCollection$toList(1, py_index) %>% ee$List$get(0))
+    s4_name <- s4_img %>% ee_get_date_img()
+    s4_names[[i]] <- as.character(s4_name$time_start)
+    s4_img <- s4_img$select(band_name)$rename(as.character(s4_name$time_start))
+    s4_img_list[[i]] <- ee$Image$pixelLonLat()$addBands(s4_img)
+    s4_img_list[[i]] <-  s4_img_list[[i]]$reduceRegion(reducer = ee$Reducer$toList(),
+                                                       geometry  = eco_mask_ee2,
+                                                       maxPixels = 1e6,
+                                                       scale = 30,
+                                                       bestEffort = TRUE)
+  }
+  
+  func_dos <- function(q) {
+  lats2 <- np$array((ee$Array(s4_img_list[[q]]$get("latitude")) %>% ee$Array$getInfo()))
+  lngs2 <- np$array((ee$Array(s4_img_list[[q]]$get("longitude")) %>% ee$Array$getInfo()))
+  
+  data.frame(x = lngs2,y = lats2)
+  evi_values4 <- list()
+  
+  for(index in  seq(nimages2$getInfo())) {
+    evi_values4[[index]] <-  ee$List(s4_img_list[[index]]$get(s4_names[[index]]))$getInfo()
+    
+  }
+  
+  #Convert list elements of evi_values into num py arrays
+  evi_values5 <- lapply(evi_values4,function(x){
+    np$array((x))
+  })
+  
+  eviss4 <- evi_values5
+  names(eviss4) <- ic_date2$id
+  evis_df_s4 <- data.frame(x = lngs2,y = lats2,lapply(eviss4, "length<-", max(lengths(eviss4))))
+  }
+  s <- func_dos(1)
+  
+}
+
+
+###################################
+t <- gee_extract()
+##############################
+
+
+
+
+
+
+
+mmm <- ee$Array(s4_img_list[[1]]$get("latitude")) %>% ee$Array$getInfo()
+
+func_dos <- function(q) {
+  lats2 <- np$array((ee$Array(s4_img_list[[q]]$get("latitude")) %>% ee$Array$getInfo()))
+  lngs2 <- np$array((ee$Array(s4_img_list[[q]]$get("longitude")) %>% ee$Array$getInfo()))
+  
+  data.frame(x = lngs2,y = lats2)
+  evi_values4 <- list()
+  
+  for(index in  seq(nimages2$getInfo())) {
+    evi_values4[[index]] <-  ee$List(s4_img_list[[index]]$get(s4_names[[index]]))$getInfo()
+    
+  }
+  
+  #Convert list elements of evi_values into num py arrays
+  evi_values5 <- lapply(evi_values4,function(x){
+    np$array((x))
+  })
+    
+    eviss4 <- evi_values5
+    names(eviss4) <- ic_date2$id
+    evis_df_s4 <- data.frame(x = lngs2,y = lats2,lapply(eviss4, "length<-", max(lengths(eviss4))))
+    
+}
+  
+
+
+
+
+
+
+e <- seq(nimages2$getInfo())
+evi_values4 <- list()
+evi_values5 <- lapply(e,function(x){
+  evi_values4[[1]] <-  np$array((ee$List(s4_img_list[[1]]$get(s4_names[[1]]) %>% ee$List$getInfo())))
+  
+})
+
+evi_values5 <- lapply(e,function(x){
+evi_values4[[index]] <-  ee$List(s4_img_list[[x]]$get(s4_names[[x]]))$getInfo()
+})
+
+
+
+
+
+
+evi_values5 <- lapply(evi_values5,function(x){
+  np$array((x))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gee_extract()
+
+mmm <- np$array((ee$Array(s4_img_list[[1]]$get("latitude")) %>% ee$Array$getInfo()))
+
+lats2 <-  np$array((ee$Array(s4_img_list[[1]]$get("latitude"))))
+                   
+lats2$getInfo          
+
+
+
+
+
+
+%>% ee$Array$get(0:1000)))
+as.list(mmm)
+xx <- np$array(mmm$getInfo())
+as.array((mmm))
+
+
+lats <-  np$array((ee$Array(s2_img_list[[1]]$get("latitude"))$getInfo()))
+lngs2 <- np$array((ee$Array(s4_img_list[[1]]$get("longitude"))$getInfo()))
+#For smaller region: Obtain the EVI values and clean the results of the google earth engine list
+
+#For loop to extract all the iamges from the s2_img_list list containing them and converting them to ee.List elements storing them in the evi_values2 list. 
+
+for(index in seq(nimages2$getInfo())) {
+  evi_values4[[index]] <-  ee$List(s4_img_list[[index]]$get(s4_names[[index]]))$getInfo()
+  
+}
+
+
+
+#   
+# table <- ee$FeatureCollection('USDOS/LSIB_SIMPLE/2017')
+#   
+# print(table$filter(ee$Filter$eq('country_na', 'Niger'))$first()$getInfo())
+# 
+#   
+#   
+#   
+  
+  
+
+ff <- ee$List
+
+mynewlist = evi_values4$map(ee_utils_pyfunc(function(x) {ee$List(s2_img_list[[x]]$get(s2_names[[x]]))$getInfo()}
+  )
+)
+
+s2_names[[2]]$
+
+#Convert list elements of evi_values into num py arrays
+evi_values5 <- lapply(evi_values4,function(x){
+  np$array((x))
+})
+
+#Create copy of evi values and rename the columns of the elements
+eviss4 <- evi_values5
+names(eviss4) <- ic_date2$id
+
+
+
+
+### Create Dataframe for Smaller Ecoregion; average the columns with NA values; and rename the columns; 
+evis_df_s4 <- data.frame(x = lngs2,y = lats2,lapply(eviss4, "length<-", max(lengths(eviss4))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Lapply method -- takes same amount of time 1:37 for 36 images. 
+# evi_values4 <- list()
+# evi_values5 <- lapply(seq_len(nimages),function(x){
+#   evi_values4[[x]] <-  ee$List(s2_img_list[[x]]$get(s2_names[[x]]))$getInfo()
+# })
+# 
+# 
+
+
+#Create copy of evi values and rename the columns of the elements
+eviss4 <- evi_values5
+names(eviss4) <- ic_date2$id
+
+
+
+
+### Create Dataframe for Smaller Ecoregion; average the columns with NA values; and rename the columns; 
+evis_df_s4 <- data.frame(x = lngs2,y = lats2,lapply(eviss4, "length<-", max(lengths(eviss4))))
+
+
+pos <- grep(pattern = "NA", x = evis_df_s4)
+while(length(pos)>0){
+  for(t in pos){
+    if(t < ncol(evis_df_s4)){
+      evis_df_s4[t] <- (evis_df_s4[t+1]-evis_df_s4[t-1])/2
+    }else
+      evis_df_s4[t] <- evis_df_s4[ncol(evis_df_s4)-1]
+  }
+  pos <- grep(pattern = "NA", x = evis_df_s4)
+  
+}
+
+
+coords3 <-evis_df_s4 %>% select(x, y) 
+
+evis_df_s4 <- evis_df_s4 %>% select (-c(x, y))
+
+
+evis_df_s4[evis_df_s4 < 0] = 0
+
+evis_df_s4_2 <- data.frame(coords3,evis_df_s4)
+
+
+
+
+idcy <-  ic_date2$time_start
+dates <- ymd(idcy)
+
+
+#add reformatted dates
+
+names(evis_df_s4_2)[3:length(evis_df_s4_2)] <- as.character(dates)
+evis_df_s4_2 <- evis_df_s4_2 %>%
+  mutate_all(as.numeric)
+
+
+
+
+#Extract years from dates 
+y2 <- data.frame(Date=(seq(dates[1], dates[length(dates)], by = 'year')))
+y2$Year<-year(y2$Date)
+years <- y2$Year
+years[length(years)]
+
+
+stDate <- c(as.numeric(min(years)),01,01)
+
+
+
+
+all_pixels_2 <- evis_df_s4_2 %>% 
+  select (-c(x, y)) %>%
+  arrange() %>% 
+  slice(1:nrow(evis_df_s4_2)) %>% 
+  t() 
+
+
+# #create time series 
+# 
+
+EVIseries2 <- ts(all_pixels_2, start=c(years[1], 1), end=c(years[length(years)]
+                                                           , 12), frequency=12)
+
+
+#Create empty lists for lapply
+Yt_interpolate_m3 <- list()
+Yt_interpolate_m4 <- list()
+
+
+#Apply Tspp # time series pre-processing ---interpolating across whole data set.  Use lapply to retain time series information. 
+ww <- ncol(EVIseries2)
+Yt_interpolate_m4 <- lapply(seq(ww), function(x){
+  Yt_interpolate_m3[[x]]  <- TsPP(EVIseries2[,x], tsgf=TSGFspline)
+})
+
+
+
+#Create empty loop for list
+Phen_2000_2007_3 <- list()
+
+for(i in seq(ww)){
+  Phen_2000_2007_3[[i]] <- Phenology(Yt_interpolate_m4[[i]], approach="White")
+}
+
+
+
+#Create empty list containers for loop
+sos2 <- list()
+eos2 <- list()
+los2 <- list()
+pop2 <- list()
+
+
+for(i in seq(ncol(EVIseries2))){
+  sos2[[i]] <-  as.numeric( Phen_2000_2007_3[[i]][["sos"]])
+  eos2[[i]] <-  as.numeric( Phen_2000_2007_3[[i]][["eos"]])
+  los2[[i]] <-  as.numeric( Phen_2000_2007_3[[i]][["los"]])
+  pop2[[i]] <-  as.numeric( Phen_2000_2007_3[[i]][["pop"]])
+  
+}
+
+plot(Yt_interpolate_m4[[1]])
+
+sos2 <- unlist(sos2)
+eos2 <- unlist(eos2)
+los2 <- unlist(los2)
+pop2 <- unlist(pop2)
+
+
+coords3 <-evis_df_s4_2 %>% select(x, y) 
+coords4 <- coords3 %>% slice(rep(1:n(), each = 8))
+
+
+DF_phenmet_test2 <- data.frame(coords4,sos2, eos2, los2, pop2) %>% cbind(years)
+
+DF_phenmet_testarranged2 <- DF_phenmet_test2 %>% arrange(years)
+
+
+DF_phenmet_testarranged_sf2 = st_as_sf(DF_phenmet_testarranged2, coords = c("x", "y"), crs = 4326)
+
+
+year_chars2 <- as.character(years)
+#year_chars[1]
+
+separated_rasters2 <- list()
+# x <- length(years)
+
+for(g in seq(years)){
+  separated_rasters2[[g]] <- lapply(years[g], function(x){DF_phenmet_testarranged2 %>% filter(years == year_chars2[g])})
+}
+
+DF_phenmet_testarranged2 %>% filter(years == year_chars2[1])
+rasterList2 <- brick()
+rasterList2 <- lapply(1:length(separated_rasters2), function(x){rasterFromXYZ(separated_rasters2[[x]][[1]])})
+
+rasterBrick2 <- brick(rasterList2)
+
+plot(rasterBrick2$pop2.3)
